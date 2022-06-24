8c43fe9199874585a3c9e2db1293ab08b7813c1e
HelloShaohua <wu.shaohua@foxmail.com>
2022-06-24 09:45:06 +0800

    perf: version (#1)

--- version.go
+++ version.go
@@ -1,83 +1,45 @@
-// Package version supplies version information collected at build time to
-// apimachinery components.
 package version
 
 import (
-	"fmt"
-	"runtime"
+	"bytes"
+	"encoding/json"
 
-	"github.com/coolstina/json"
-	"github.com/gosuri/uitable"
+	formatalign "github.com/coolstina/format-align"
 )
 
-var (
-	// GitVersion is semantic version.
-	GitVersion = "v0.0.0-master+$Format:%h$"
-	// BuildDate in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ').
-	BuildDate = "1970-01-01T00:00:00Z"
-	// GitCommit sha1 from git, output of $(git rev-parse HEAD).
-	GitCommit = "$Format:%H$"
-	// GitTreeState state of git tree, either "clean" or "dirty".
-	GitTreeState = ""
-)
-
-// Info contains versioning information.
-type Info struct {
-	GitVersion   string `json:"GitVersion"`
-	GitCommit    string `json:"GitCommit"`
-	GitTreeState string `json:"GitTreeState"`
-	BuildDate    string `json:"BuildDate"`
-	GoVersion    string `json:"GoVersion"`
-	Compiler     string `json:"Compiler"`
-	Platform     string `json:"Platform"`
+// Millet builder information structure.
+type Millet struct {
+	AppName    string
+	Version    string
+	GoVersion  string
+	GitCommit  string
+	GitBranch  string
+	DestOSARCH string // Destination OS linux/amd64
+	FromOSARCH string // Building from darwin/amd64
+	BuildTime  string
 }
 
-// String returns info as a human-friendly version string.
-func (info Info) String() string {
-	if s, err := info.Text(); err == nil {
-		return string(s)
-	}
-
-	return info.GitVersion
+func (millet *Millet) JSONString() string {
+	data, _ := json.MarshalIndent(&millet, "", "  ")
+	return string(data)
 }
 
-// ToJSON returns the JSON string of version information.
-func (info Info) ToJSON() string {
-	s, _ := json.Marshal(info)
+func (millet *Millet) RawString() string {
+	var buffer bytes.Buffer
+	var format = formatalign.NewFormatAlign(
+		formatalign.WithAlignment(formatalign.AlignmentOfRight),
+		formatalign.WithSeparator(""),
+		formatalign.WithPlaceholder(12),
+	)
 
-	return string(s)
-}
+	buffer.Write(append([]byte(format.Format("AppName:", millet.AppName)), '\n'))
+	buffer.Write(append([]byte(format.Format("Version:", millet.Version)), '\n'))
+	buffer.Write(append([]byte(format.Format("GoVersion:", millet.GoVersion)), '\n'))
+	buffer.Write(append([]byte(format.Format("GitCommit:", millet.GitCommit)), '\n'))
+	buffer.Write(append([]byte(format.Format("GitBranch:", millet.GitBranch)), '\n'))
+	buffer.Write(append([]byte(format.Format("DestOSARCH:", millet.DestOSARCH)), '\n'))
+	buffer.Write(append([]byte(format.Format("FromOSARCH:", millet.FromOSARCH)), '\n'))
+	buffer.Write(append([]byte(format.Format("BuildTime:", millet.BuildTime)), '\n'))
 
-// Text encodes the version information into UTF-8-encoded text and
-// returns the result.
-func (info Info) Text() ([]byte, error) {
-	table := uitable.New()
-	table.RightAlign(0)
-	table.MaxColWidth = 80
-	table.Separator = " "
-	table.AddRow("GitVersion:", info.GitVersion)
-	table.AddRow("GitCommit:", info.GitCommit)
-	table.AddRow("GitTreeState:", info.GitTreeState)
-	table.AddRow("BuildDate:", info.BuildDate)
-	table.AddRow("GoVersion:", info.GoVersion)
-	table.AddRow("Compiler:", info.Compiler)
-	table.AddRow("Platform:", info.Platform)
-
-	return table.Bytes(), nil
-}
-
-// Get returns the overall codebase version. It's for detecting
-// what code a binary was built from.
-func Get() Info {
-	// These variables typically come from -ldflags settings and in
-	// their absence fallback to the settings in pkg/version/base.go
-	return Info{
-		GitVersion:   GitVersion,
-		GitCommit:    GitCommit,
-		GitTreeState: GitTreeState,
-		BuildDate:    BuildDate,
-		GoVersion:    runtime.Version(),
-		Compiler:     runtime.Compiler,
-		Platform:     fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
-	}
+	return buffer.String()
 }
--- version_test.go
+++ version_test.go
@@ -8,26 +8,55 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-func TestVersionSuite(t *testing.T) {
-	suite.Run(t, &VersionSuite{})
+func TestMilletSuite(t *testing.T) {
+	suite.Run(t, &MilletSuite{})
 }
 
-type VersionSuite struct {
+type MilletSuite struct {
 	suite.Suite
-	info Info
+	millet *Millet
 }
 
-func (suite *VersionSuite) BeforeTest(suiteName, testName string) {
-	suite.info = Get()
+func (suite *MilletSuite) BeforeTest(suiteName, testName string) {
+	suite.millet = &Millet{
+		AppName:    "millet",
+		Version:    "v1.2.3-beta",
+		GoVersion:  "go1.18.3",
+		GitCommit:  "fb557a8447f471f394cd637b6ae86c5612d7998c",
+		GitBranch:  "release",
+		DestOSARCH: "linux/amd64",
+		FromOSARCH: "darwin/amd64",
+		BuildTime:  "2022-06-23T16:57:14+08:00",
+	}
 }
 
-func (suite *VersionSuite) Test_Info_String() {
-	actual := suite.info.String()
-	assert.NotEmpty(suite.T(), actual)
+func (suite *MilletSuite) Test_RawString() {
+	expected := `    AppName: millet
+    Version: v1.2.3-beta
+  GoVersion: go1.18.3
+  GitCommit: fb557a8447f471f394cd637b6ae86c5612d7998c
+  GitBranch: release
+ DestOSARCH: linux/amd64
+ FromOSARCH: darwin/amd64
+  BuildTime: 2022-06-23T16:57:14+08:00
+`
+	actual := suite.millet.RawString()
+	assert.Equal(suite.T(), expected, actual)
 	fmt.Println(actual)
 }
 
-func (suite *VersionSuite) Test_Info_ToJSON() {
-	actual := suite.info.ToJSON()
-	assert.NotEmpty(suite.T(), actual)
+func (suite *MilletSuite) Test_JSONString() {
+	expected := `{
+  "AppName": "millet",
+  "Version": "v1.2.3-beta",
+  "GoVersion": "go1.18.3",
+  "GitCommit": "fb557a8447f471f394cd637b6ae86c5612d7998c",
+  "GitBranch": "release",
+  "DestOSARCH": "linux/amd64",
+  "FromOSARCH": "darwin/amd64",
+  "BuildTime": "2022-06-23T16:57:14+08:00"
+}`
+	actual := suite.millet.JSONString()
+	assert.Equal(suite.T(), expected, actual)
+	fmt.Println(actual)
 }
